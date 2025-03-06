a533c117b26c6fc09aa4df68e5fbd045b130e4ca
Abhi-Karani <abhi.neilkarani@facctum.com>
2025-03-06 17:45:02 +0530

    json files

--- main.py
+++ main.py
@@ -13,22 +13,22 @@ pdf_path = f"PDF/{filename}.pdf"
 # with open(f"pdf_plumber/Results/{filename}_reverse.json", "w") as file:
 #     json.dump(json_output, file, ensure_ascii=False, indent=4)
 
-with open(f"pdf_plumber/Results/{filename}_reverse.json", "r") as file:
-    json_output = json.load(file)
-
-cleaned_output = clean_json_tables(json_output)
-with open(f"pdf_plumber/Results/cleaned/{filename}_cleaned.json", "w") as file:
-    json.dump(cleaned_output, file, ensure_ascii=False, indent=4)
-
-# print("started")
 # with open(f"pdf_plumber/Results/{filename}_reverse.json", "r") as file:
-#     a = json.load(file)
+#     json_output = json.load(file)
+
+# cleaned_output = clean_json_tables(json_output)
+# with open(f"pdf_plumber/Results/cleaned/{filename}_cleaned.json", "w") as file:
+#     json.dump(cleaned_output, file, ensure_ascii=False, indent=4)
+
+print("started")
+with open(f"pdf_plumber/Results/cleaned/{filename}_cleaned.json", "r") as file:
+    a = json.load(file)
     
 
-# content, context = extract_records(a)
-# with open(f"context_extractor/{filename}_input.txt", "w") as file:
-#         json.dump({"input":str(content)}, file, ensure_ascii=False, indent=4)
+content, context = extract_records(a)
+with open(f"context_extractor/{filename}_input.txt", "w") as file:
+        json.dump({"input":str(content)}, file, ensure_ascii=False, indent=4)
 
-# with open(f"context_extractor/{filename}_output.json", "w") as file:
-#         json.dump(context, file, ensure_ascii=False, indent=4)
+with open(f"context_extractor/{filename}_output.json", "w") as file:
+        json.dump(context, file, ensure_ascii=False, indent=4)
         
